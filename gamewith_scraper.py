2d5ef42f1e85dc483aaf386325c6455667334f48
roppy001 <roppy.dreamvision200@gmail.com>
2023-03-02 01:11:24 +0900

    各因子設定を実装

--- gamewith_scraper.py
+++ gamewith_scraper.py
@@ -42,30 +42,92 @@ def scrape(gw_config, search, driver):
         daihyo_element.find_element(By.CSS_SELECTOR,
             "#mainOnly").click()
         
-
-
-
-
     # 青因子選択位置にスクロール
     blue_element = friends_list_sr.find_element(By.CSS_SELECTOR,
         "div.-r-common-modal__container ul > li:nth-of-type(5)")
     common.scroll_element(driver, blue_element)
     time.sleep(1)
 
-    blue2_select = Select(friends_list_sr.find_element(By.CSS_SELECTOR,
-        "div.-r-common-modal__container ul > li:nth-of-type(5) > div > div:nth-of-type(4) select"))
-    blue2_select.select_by_visible_text("☆3")
+    # スピード因子
+    tmp = search[common.SEARCH_SPEED_KEY]
+    if tmp!=0:
+        Select(blue_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(1) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+    time.sleep(1)
+    # スタミナ因子
+    tmp = search[common.SEARCH_STAMINA_KEY]
+    if tmp!=0:
+        Select(blue_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(2) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+    time.sleep(1)
+    # パワー因子
+    tmp = search[common.SEARCH_POWER_KEY]
+    if tmp!=0:
+        Select(blue_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(3) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+    # 根性因子
+    tmp = search[common.SEARCH_GUTS_KEY]
+    if tmp!=0:
+        Select(blue_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(4) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 賢さ因子
+    tmp = search[common.SEARCH_WISDOM_KEY]
+    if tmp!=0:
+        Select(blue_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(5) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
     time.sleep(1)
 
-
+    # 赤因子選択位置にスクロール
     red_element = friends_list_sr.find_element(By.CSS_SELECTOR,
         "div.-r-common-modal__container ul > li:nth-of-type(6)")
     common.scroll_element(driver, red_element)
     time.sleep(1)
 
-    redb1_select = Select(friends_list_sr.find_element(By.CSS_SELECTOR,
-        "div.-r-common-modal__container ul > li:nth-of-type(6) > div > div:nth-of-type(5) select"))
-    redb1_select.select_by_visible_text("☆3")
+    # 芝因子
+    tmp = search[common.SEARCH_TURF_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(1) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # ダート因子
+    tmp = search[common.SEARCH_DIRT_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(2) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 短距離因子
+    tmp = search[common.SEARCH_SHORT_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(3) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # マイル因子
+    tmp = search[common.SEARCH_MILE_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(4) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 中距離因子
+    tmp = search[common.SEARCH_MIDDLE_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(5) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 長距離因子
+    tmp = search[common.SEARCH_LONG_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(6) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 逃げ因子
+    tmp = search[common.SEARCH_NIGE_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(7) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 先行因子
+    tmp = search[common.SEARCH_SENKOU_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(8) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 差し因子
+    tmp = search[common.SEARCH_SASHI_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(9) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
+
+    # 追込因子
+    tmp = search[common.SEARCH_OIKOMI_KEY]
+    if tmp!=0:
+        Select(red_element.find_element(By.CSS_SELECTOR,"div > div:nth-of-type(10) > div > select")).select_by_visible_text(FACTOR_STRINGS[tmp])
     time.sleep(1)
 
     search_element = friends_list_sr.find_element(By.CSS_SELECTOR,
@@ -77,6 +139,17 @@ def scrape(gw_config, search, driver):
     time.sleep(1)
 
     for i in range(max_next_count):
+        if i!=0:
+            try:
+                result_next = friends_list_sr.find_element(By.CSS_SELECTOR, "div > div.-r-umamusume-friends-list__list-wrap > button.-r-umamusume-friends-button")
+                common.scroll_element(driver, result_next)
+                common.scroll(driver, 0, -100)
+                time.sleep(1)
+                result_next.click()
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
         result_player_elements = friends_list_sr.find_elements(By.CSS_SELECTOR, "div > div.-r-umamusume-friends-list__list-wrap > ul > li > div.-r-umamusume-friends-list-item__contents")
         time.sleep(1)
 
@@ -93,15 +166,6 @@ def scrape(gw_config, search, driver):
             print(str)
 
 
-        try:
-            result_next = friends_list_sr.find_element(By.CSS_SELECTOR, "div > div.-r-umamusume-friends-list__list-wrap > button.-r-umamusume-friends-button")
-            common.scroll_element(driver, result_next)
-            common.scroll(driver, 0, -100)
-            time.sleep(1)
-            result_next.click()
-            time.sleep(1)
-        except NoSuchElementException:
-            break
 
     return
     
