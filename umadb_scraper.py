bd59862a4266d832a6f46c290c70756ad1ea9fa4
roppy001 <roppy.dreamvision200@gmail.com>
2023-03-16 04:10:21 +0900

    ウマ娘DBデザイン変更に対応

--- umadb_scraper.py
+++ umadb_scraper.py
@@ -45,29 +45,28 @@ async def scrape(config, search, driver, id_history_list):
         if tmp!=0:
             blue_factor_list.append((k, tmp))
 
-    common.scroll_element(driver, driver.find_element(By.CSS_SELECTOR,"#__BVID__34"))
+    common.scroll_element(driver, driver.find_element(By.CSS_SELECTOR,"#blue_factor"))
 
     i = 1
     for blue_factor in blue_factor_list:
         blue_add_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__34 > div > button")
+            "#__BVID__32 > div > button")
         blue_add_element.click()
         await asyncio.sleep(1)
 
         blue_type_pulldown_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__34 > div > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__field.js-tag-for-autofocus > i")
+            "#blue_factor > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__field.js-tag-for-autofocus > i")
         blue_type_pulldown_element.click()
         await asyncio.sleep(1)
 
         blue_type_item_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__34 > div > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__options > div:nth-child("+BLUE_TYPE_INDEX_STR[blue_factor[0]]+")")
+            "#blue_factor > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__options > div:nth-child("+BLUE_TYPE_INDEX_STR[blue_factor[0]]+")")
         blue_type_item_element.click()
 
         Select(driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__34 > div > div:nth-child("+str(i)+") > div:nth-of-type(2) > select")).select_by_value(str(blue_factor[1]))
-
+            "#blue_factor > div:nth-child("+str(i)+") > div:nth-of-type(2) > select")).select_by_value(str(blue_factor[1]))
         #Select(driver.find_element(By.CSS_SELECTOR,
-        #    "#__BVID__34 > div > div:nth-child("+str(i)+") > div:nth-of-type(3) > select")).select_by_value("1")
+        #    "#blue_factor > div:nth-child("+str(i)+") > div:nth-of-type(3) > select")).select_by_value("1")
 
         i = i + 1
 
@@ -79,34 +78,34 @@ async def scrape(config, search, driver, id_history_list):
         if tmp!=0:
             red_factor_list.append((k, tmp))
 
-    common.scroll_element(driver, driver.find_element(By.CSS_SELECTOR,"#__BVID__35"))
+    common.scroll_element(driver, driver.find_element(By.CSS_SELECTOR,"#red_factor"))
 
     i = 1
     for red_factor in red_factor_list:
         red_add_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__35 > div > button")
+            "#__BVID__33 > div > button")
         red_add_element.click()
         await asyncio.sleep(1)
 
         red_type_pulldown_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__35 > div > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__field.js-tag-for-autofocus > i")
+            "#red_factor > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__field.js-tag-for-autofocus > i")
         red_type_pulldown_element.click()
         await asyncio.sleep(1)
 
         red_type_item_element = driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__35 > div > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__options > div:nth-child("+RED_TYPE_INDEX_STR[red_factor[0]]+")")
+            "#red_factor > div:nth-child("+str(i)+") > div:nth-of-type(1) > div > div > div.gb-field-select__options > div:nth-child("+RED_TYPE_INDEX_STR[red_factor[0]]+")")
         red_type_item_element.click()
 
         Select(driver.find_element(By.CSS_SELECTOR,
-            "#__BVID__35 > div > div:nth-child("+str(i)+") > div:nth-of-type(2) > select")).select_by_value(str(red_factor[1]))
+            "#red_factor > div:nth-child("+str(i)+") > div:nth-of-type(2) > select")).select_by_value(str(red_factor[1]))
 
         #Select(driver.find_element(By.CSS_SELECTOR,
-        #    "#__BVID__35 > div > div:nth-child("+str(i)+") > div:nth-of-type(3) > select")).select_by_value("1")
+        #    "#red_factor > div:nth-child("+str(i)+") > div:nth-of-type(3) > select")).select_by_value("1")
 
         i = i + 1
 
     search_element = driver.find_element(By.CSS_SELECTOR,
-            "#app > div > div > div.btn-group > button.btn.btn-primary")
+            "#app > div > div > div.btn-group > button.btn.btn-success")
 
     common.scroll_element(driver, search_element)
     search_element.click()
